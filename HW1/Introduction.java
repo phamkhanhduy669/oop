bc71fc8c9d58cbfc5581feedfa2ce15befab3488
phamkhanhduy <phamkhanhduy669@gmail.com>
2024-09-21 09:08:13 +0700

    commit Introduction.java

---
+++ HW1/Introduction.java
@@ -0,0 +1,9 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Pham Khanh Duy\t23020522\tK68I-IS\tphamkhanhduy669\t23020522@vnu.edu.vn");
+        for(int i = 9 ; i > 0;--i) {
+            System.out.println(i + " bottles of beer on the wall, "+ i +" bottles of beer."+'\n'+"Take one down, pass it around,");
+        }
+        System.err.println("No more bottles of beer on the wall.");
+    }
+}
